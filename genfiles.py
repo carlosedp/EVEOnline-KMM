b339a15ff72235b039a477acf5bdd2af62d35232
carlosedp <carlosedp@5fedec92-9c1a-0410-9b01-334d63f77eb0>
2006-12-08 15:08:29 +0000

    Moved remotely

---
+++ genfiles.py
@@ -0,0 +1,60 @@
+#############################################################################
+##
+## Copyright (C) 2006-2006 Kavanagh Productions. All rights reserved.
+##
+## This file is part of the Kavanagh Productions Projects
+## Find more information in: http://kavanaghprod.googlepages.com
+## Contact at kavanaghprod@gmail.com
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following information to ensure GNU
+## General Public Licensing requirements will be met:
+## http://www.gnu.org/licenses/gpl.txt
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+## $Rev$
+## $Date$
+##
+#############################################################################
+
+import os
+import glob
+import time
+
+class processFiles(object):
+    def __init__(self, pat, command):
+            self.pat = pat
+            self.command = command
+            self.pathWalk()
+
+    def pathWalk(self):
+        arglist = []
+        os.path.walk("./",self.process,arglist)
+
+    def process(self, arg, dirname, fnames):
+        for file in fnames:
+            if file.endswith(str(self.pat)):
+                if self.pat == ".qrc":
+                    outfile = file.split(".")[0] + "_rc.py"
+                else:
+                    outfile = file.split(".")[0] + ".py"
+                outfile = os.path.join(dirname, outfile)
+                outcommand = self.command + " " + os.path.join(dirname, file) + " -o " + outfile
+                print "File " + outfile + " created."
+                os.system(outcommand)    
+
+uifiles = processFiles(".ui", "pyuic4")
+resfiles = processFiles(".qrc", "pyrcc4")
+print ""
+
+#Translation only
+#os.system("pylupdate4 -verbose ./KMM.pro")
+#os.system("lrelease -verbose ./KMM.pro")
+
+print ""
+print "QT files generation done"
+time.sleep(1)
---
+++ xmlparse2.py
@@ -0,0 +1,85 @@
+#############################################################################
+##
+## Copyright (C) 2006-2006 Kavanagh Productions. All rights reserved.
+##
+## This file is part of the Kavanagh Manufacture Manager - KMM.
+## Find more information in: http://kavanaghprod.googlepages.com
+## Contact at kavanaghprod@gmail.com
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following information to ensure GNU
+## General Public Licensing requirements will be met:
+## http://www.gnu.org/licenses/gpl.txt
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+## $Rev$
+## $Date$
+##
+#############################################################################
+import sys
+import xml.etree.ElementTree as ET
+import urllib
+import re
+from PyQt4 import QtCore, QtGui
+
+from utilities import *
+
+def parseXMLSkills(inFile):
+    etree = ET.parse(inFile)
+
+    root = etree.getroot()
+    chars = root.find('characters')
+    char = chars.findall('character')
+    if char[0].find('skills') != None:
+        char = char[0]
+    elif char[1].find('skills') != None:
+        char = char[1]
+    else:
+        char = char[2]
+
+    skills = char.find('skills')
+    for skillGroup in skills.findall('skillGroup'):
+        for skill in skillGroup.findall('skill'):
+            yield(skill.get('typeName') + '|' + skill.get('typeID') + '|' + skill.find('level').text)
+
+def parseMinsStandardXML(index):
+    """
+    Grabs the mineral prices from standard EVE-Mon XML
+    http://www.phoenix-industries.org - http://www.phoenix-industries.org/evemonprices.xml
+    http://eve.battleclinic.com - http://eve.battleclinic.com/eve_online/market.php?feed=xml
+    """
+    indexes = {'phoenix': 'http://www.phoenix-industries.org/evemonprices.xml',
+               'battleclinic': 'http://eve.battleclinic.com/eve_online/market.php?feed=xml',
+               'EVE-Central': 'http://eve-central.com/home/marketstat_xml.html?evemon=1'}
+    
+    settings = QtCore.QSettings("Kavanagh Productions", "KMM")
+    proxy = str(settings.value("proxy", QtCore.QVariant('')).toString())
+    proxies = {'http': proxy}
+    urlopener = urllib.FancyURLopener(proxies)
+    xml = urlopener.open(indexes[index])
+    #print xml.read()
+    etree = ET.parse(xml)
+    root = etree.getroot()
+    mins = root.findall('mineral')
+    for min in mins:
+        yield(min.find('name').text + '|' + money_me(float(min.find('price').text)))
+
+def parseMinQuant():
+    """
+    Grabs the mineral prices from Quant Corporation
+    http://www.starvingpoet.net/
+    """
+    settings = QtCore.QSettings("Kavanagh Productions", "KMM")
+    proxy = str(settings.value("proxy", QtCore.QVariant('')).toString())
+    proxies = {'http': proxy}
+    urlopener = urllib.FancyURLopener(proxies)
+    xml = urlopener.open('http://www.starvingpoet.net/includes/minerals.php').read()
+
+    regexp = re.compile(r'<TD WIDTH="\d*px">(\w*)</TD><TD WIDTH=.*?>([0-9\.\,]*)</TD><TD.*?>[0-9\.\,]*</TD>')
+    m = regexp.findall(xml)
+    for i in xrange(8):
+        yield(m[i][0] + '|' + m[i][1])
